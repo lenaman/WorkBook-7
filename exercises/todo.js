c7e9c4f0e0c77eea502a56038dc06599ff7791d6
Lena Man <manlena01@gmail.com>
2024-11-19 15:28:38 -0500

    to do js

--- exercises/todo.js
+++ exercises/todo.js
@@ -1,3 +1,13 @@
+
+let idInput = document.querySelector("#idInput")
+async function getToDo(){
+    let promise = fetch("https://jsonplaceholder.typicode.com/todos/1");
+    let response = await promise
+    let data = await response.json();
+    console.log(todo);
+    }
+    getToDo();
+
 function showDetails(){
     console.log("Hello, how are you?")
 }
